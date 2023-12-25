fb3049c2a9570f928b764fc42603d532e9abed48
yash3567 <yashnandeshwar32@gmail.com>
2023-12-25 14:23:48 +0530

    updates

--- Assignmentoperator.java
+++ Assignmentoperator.java
@@ -1,32 +1,45 @@
-class assignmentoperator {
+// class assignmentoperator {
 
-    public static void main(String[] args) {
-        // int num1 = 5;
-        // int num2 = 3;
-        // int result = num1 + num2;
-        // System.out.println(result);
+//     public static void main(String[] args) {
+//         // int num1 = 5;
+//         // int num2 = 3;
+//         // int result = num1 + num2;
+//         // System.out.println(result);
 
-        // int num4 = 5;
-        // int num5 = 3;
-        // int resul = num4 - num5;
-        // System.out.println(resul);
+//         // int num4 = 5;
+//         // int num5 = 3;
+//         // int resul = num4 - num5;
+//         // System.out.println(resul);
 
-        // int num6 = 5;
-        // int num7 = 3;
-        // int resu = num6 * num7;
-        // System.out.println(resu);
+//         // int num6 = 5;
+//         // int num7 = 3;
+//         // int resu = num6 * num7;
+//         // System.out.println(resu);
 
-        // int num8 = 5;
-        // int num9 = 3;
-        // int res = num8 / num9;
-        // System.out.println(res);
+//         // int num8 = 5;
+//         // int num9 = 3;
+//         // int res = num8 / num9;
+//         // System.out.println(res);
 
-        // int num = 10;
-        // num = num + 10;
-        // System.out.println(num);
-        int num = 20;
-        num += 10;
-        System.out.println(num);
+//         // int num = 10;
+//         // num = num + 10;
+//         // System.out.println(num);
+//         int num = 20;
+//         num += 10;
+//         System.out.println(num);
 
+//     }
+// }
+/**
+ * Assignmentoperator
+ */
+public class Assignmentoperator {
+
+    public static void main(String args[]) {
+
+        int num = 50;
+        int var = 100;
+        int result = num - var;
+        System.out.println(result);
     }
 }
