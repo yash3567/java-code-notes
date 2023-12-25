229269274e47c27e18f4fd0e00837b6adf329035
yash3567 <yashnandeshwar32@gmail.com>
2023-12-25 14:41:27 +0530

    Add Practice Code

---
+++ practice.java
@@ -0,0 +1,11 @@
+class practice {
+
+    public static void main(String args[]) {
+
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("yash" + i);
+            i++;
+        }
+    }
+}
