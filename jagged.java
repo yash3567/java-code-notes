484970cdd28ef4a808bf2a07bddde9a8870f5ab5
yash3567 <yashnandeshwar32@gmail.com>
2023-12-27 13:42:19 +0530

    updates

--- jagged.java
+++ jagged.java
@@ -4,39 +4,13 @@ class jagged {
 
         int nums[][][] = new int[3][4][5];
 
-        // nums[0] = new int[4];
-        // nums[1] = new int[3];
-        // nums[2] = new int[2];
-
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums[i].length; j++)
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++)
 
             {
-                nums[i][j] = (int) (Math.random() * 10);
-            }
-
-        }
-
-        // for (int i = 0; i < 3; i++) {
-        // for (int j = 0; j < 4; j++)
-
-        // {
-        // System.out.print(nums[i][j] + "");
-
-        // }
-
-        // System.out.println();
-        // }
-        for (int n[] : nums)
-
-        {
-            for (int m : n)
-
-            {
-                System.out.print(m + "");
+                System.out.print(nums[i][j] + "");
 
             }
-            System.out.println();
 
         }
     }
