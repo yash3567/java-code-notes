c5107a94424a2058f2a9f536afb509b117c49c00
yash3567 <yashnandeshwar32@gmail.com>
2023-12-27 13:34:43 +0530

    updates

--- practice.java
+++ practice.java
@@ -12,6 +12,10 @@ class practice {
             System.out.println("yash" + j);
             i++;
         }
-
+        int k = 1;
+        while (i <= 5) {
+            System.out.println("yash" + k);
+            i++;
+        }
     }
 }
