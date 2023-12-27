6b6eba1d55286e8505a9137ab81b5f7255fe4498
yash3567 <yashnandeshwar32@gmail.com>
2023-12-27 13:39:57 +0530

    java-code-notes

--- practice.java
+++ practice.java
@@ -7,15 +7,5 @@ class practice {
             System.out.println("yash" + i);
             i++;
         }
-        int j = 1;
-        while (i <= 5) {
-            System.out.println("yash" + j);
-            i++;
-        }
-        int k = 1;
-        while (i <= 5) {
-            System.out.println("yash" + k);
-            i++;
-        }
     }
 }
