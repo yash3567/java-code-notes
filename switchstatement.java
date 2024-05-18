621a3cfd0823c388458e3609f2430e5537eebf78
yash3567 <yashnandeshwar32@gmail.com>
2024-05-19 00:46:21 +0530

    updated

--- switchstatement.java
+++ switchstatement.java
@@ -38,12 +38,6 @@ class switchstatement {
                 System.out.println("sunday");
                 break;
 
-            // default:
-            // System.out.println("invalid number");
-            case 10:
-                System.out.println("sunday");
-                break;
-
             default:
                 System.out.println("invalid number");
 
